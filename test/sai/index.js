596b1be1a8a3e26096e17d7ddd1405cb902573d9
E.Azer Koçulu <azer@kodfabrik.com>
2013-04-28 01:39:05 -0700

    ignore requiring onejs

--- test/sai/index.js
+++ test/sai/index.js
@@ -4,4 +4,8 @@ var monouchi = require('monouchi'),
     bar      = require('./bar'),
     manifest = require('./package');
 
+try {
+  require('one');
+} catch(err) {}
+
 exports.sai = monouchi.monouchi && foo.foo && bar.bar && core.core && manifest.name == 'sai';
